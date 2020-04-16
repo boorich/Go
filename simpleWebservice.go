1b4e40dd1e8b1497f0d6c2dfa51adadaa2c928a8
empea-careercriminal <empeamtk@googlemail.com>
2020-04-16 12:51:54 +0200

    updated webservice accepting query and encoding JSON response

--- simpleWebservice.go
+++ simpleWebservice.go
@@ -1,13 +1,22 @@
 package main // so this can be executed from the cli
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // any request to this service is using this handler, return is a (anonymous) function
-		w.Write([]byte("Hello World")) // make Hello World a byte array so you can the writer interface of the response writer
+		names := r.URL.Query()["name"] // return map of data with all query params the app accepted and retun anything matching "name"
+		var name string
+		if len(names) == 1 { //since this might return more than one name
+			name = names[0]
+		}
+		m := map[string]string{"name": name} //map object from string to string that can easily be serialized into a JSON string initialized to "name":name
+		enc := json.NewEncoder(w)            // pass writer interface to the new encoder
+		enc.Encode(m)                        // encode the map
+
 	})
 
 	err := http.ListenAndServe(":3000", nil) // nil makes go's standard handler run, http:ListenAndServe make err of type error
