dd2a376685ab5a2141a54584728779a4a9e3b36b
empea-careercriminal <empeamtk@googlemail.com>
2020-04-23 13:35:30 +0200

    added some functiond for request verbs and JSON parsing.

--- controllers/user.go
+++ controllers/user.go
@@ -3,15 +3,61 @@ package controllers
 import (
 	"net/http"
 	"regexp"
+	"strconv"
+
+	"github.com/empea-careercriminal/Go/models"
 )
 
 type userController struct {
 	userIDPattern *regexp.Regexp
 }
 
-// A controller to receive and process a network request
+// A controller to receive and process a network request aka Traffic Cop deciding which method to pass it on to
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) { //binding the ServeHTTP method to the UserController type. This actually implements the Handler interface only by naming the method exactly right.
-	w.Write([]byte("Hello from the User Controller")) //writing back to the HTTP response object
+	if r.URL.Path == "/users" { // this request object is the entire collection of users
+		switch r.Method { // what's the method on the incoming request?
+		case http.MethodGet: // in case of a GET request
+			uc.getAll(w, r) // pass it on to this function
+		case http.MethodPost: // in case of a POST request
+			uc.Post(w, r) // pass it on to this function
+		default:
+			w.WriteHeader(http.StatusNotImplemented) // in case none of the above verbs is used for a request
+		}
+	} else {
+		matches := uc.userIDPattern.FindAllStringSubmatch(r.URL.Path) // compiles a slice of all matching expressions as defined in the user controller like so userIDPattern: regexp.MustCompile(`^/users/(/d+)/?`)
+		if len(matches) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		id, err := strconv.Atoi(matches[1]) // make this an int
+		if err == nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+		switch r.Method {
+		case http.MethodGet:
+			uc.get(id, w)
+		case http.MethodPut:
+			uc.put(id, w, r)
+		case http.MethodDelete:
+			uc.delete(id, w)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+// retrieve all users from models layer and return back out as JSON
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJSON(models.GetUsers(), w)
+}
+
+// return single user by ID
+func (uc *userController) get(id int, w http.ResponseWriter) {
+	u, err := models.GetUsersByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJSON(u, w)
 }
 
 // Contructor Function
--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 type User struct {
 	ID        int
 	FirstName string
@@ -19,8 +24,40 @@ func GetUsers() []*User {
 
 // AddUser is invoked to ad a user
 func AddUser(u User) (User, error) { // consume type user, return either a user or an error
-	u.ID = nextID             //apply next ID
-	nextID++                  // increas next ID
-	users = append(users, &u) // append addressOf the u that came in, since this is whhat the users slice stores
+	if u.ID != 0 {
+		return User{}, errors.New("New user must not include ID or it must be set to zero.")
+	}
+	u.ID = nextID             // apply next ID
+	nextID++                  // increase next ID
+	users = append(users, &u) // append addressOf the u that came in, since this is what the users slice stores
 	return u, nil             // return the newly created user object and nil, because no error
+}
+func getUserByID(id int) (User, error) { // maybe asking for an User by a non-assigned id in collection should error properly
+	for _, u := range users { // looping through the users mapping wildcarding the iterator
+		if u.ID == id { // if the provided id in the call matches the currently tested user's ID
+			return *u, nil // return den value this pointer points at (dereferencing) and nil indicating no error happened
+		}
+	}
+	return User{}, fmt.Errorf("No valid user matching the provided ID '%v' found", id) // when no matching ID error like this
+
+}
+
+func updateUser(u User) (User, error) {
+	for i, candidate := range users {
+		if candidate.ID == u.ID {
+			users[i] = &u // set addressOf of that candidate's matching user to new value
+			return u, nil // and return user without an error
+		}
+	}
+	return User{}, fmt.Errorf("No valid user matching the provided ID '%v' found", u.ID) //incoming user's id
+}
+
+func removeUserByID(id int) error {
+	for i, u := range users {
+		if u.ID == id {
+			users = append(users[:i], users[i+1:]...) // append everything fron the slice before the user we found and after the user we found
+			return nil
+		}
+	}
+	return fmt.Errorf("No valid user matching the provided ID '%v' found", id) //incoming id param
 }
